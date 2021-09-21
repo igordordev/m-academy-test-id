bb3644ccfd94ea34b3278b25c0c3c88fb54352c1
igordordev <88446424+igordordev@users.noreply.github.com>
2021-09-21 21:57:58 +0300

    Bugs fixed
    Elements are now dynamically created from JavaScript.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const pandemicStartMap = "XX0X10010X000X010X0";
+const pandemicStartMap = "XX0X10010X000X010X010X00";
 let pandemicStartMapArr = [];
 let pandemicCutMapArr = [];
 let pandemicEndMapArr = [];
 let pandemicEndMap;
 let areaCount = 0;
 let infectedCount = 0;
-let infectedPercentage;
+let startArea = document.querySelector('.start-area');
+let endArea = document.querySelector('.end-area');
+
+if (pandemicStartMap.length < 8) {
+    document.querySelector('.quarantine-world').style.width = '600px';
+} else {
+    document.querySelector('.quarantine-world').style.width = pandemicStartMap.length * 72 + 80 + 'px';
+}
 
 pandemicStartMapArr = pandemicStartMap.split("");
 pandemicStartMapArr.forEach(function (item, i) {
     let currentId = i + 1;
+    let newBlock = document.createElement('li');
+    newBlock.id = 'area-start-' + currentId;
+    startArea.append(newBlock);
     if (item == 1) {
         document.getElementById('area-start-' + currentId).classList.add('infected');
     } else if (item == 0) {
@@ -34,6 +44,9 @@ pandemicEndMap = pandemicEndMapArr.join("X");
 pandemicEndMapArr = pandemicEndMap.split("");
 pandemicEndMapArr.forEach(function (item, i) {
     let endId = i + 1;
+    let newBlock = document.createElement('li');
+    newBlock.id = 'area-end-' + endId;
+    endArea.append(newBlock);
     if (item == 1) {
         document.getElementById('area-end-' + endId).classList.add('infected');
         areaCount++;
@@ -48,4 +61,4 @@ pandemicEndMapArr.forEach(function (item, i) {
 
 document.querySelector('.total-count').textContent = areaCount;
 document.querySelector('.infected-count').textContent = infectedCount;
-document.querySelector('.infected-percentage').textContent = infectedCount / areaCount * 100;
+document.querySelector('.infected-percentage').textContent = infectedCount / areaCount * 100 || 0;
